aeec0b3c0d1710c171ee053cf9cda33cb9fb508e
Keysiwithegg <144748815+Keysiwithegg@users.noreply.github.com>
2023-10-16 21:18:21 +0800

    Update : Added about + shadow header

--- keysi.js
+++ keysi.js
@@ -17,9 +17,22 @@ if(navClose){
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link') 
 
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    //when we click on nav__link, we remove the show-menu
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click',linkAction))
 
 /*=============== SHADOW HEADER ===============*/
+const shadowHeader = () =>{
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('shadow-header')
+                        : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader)
 
 
 /*=============== EMAIL JS ===============*/
